2e85e0a7375688775a114bd8dc0d074acd0f9715
Prabal Pathak <prabal01pathak@gmail.com>
2023-02-13 06:33:08 +0000

    added websocket gin

--- go-api/app.go
+++ go-api/app.go
@@ -1,14 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	// solve cross domain problems
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func ws(c *gin.Context) {
+	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Print("read", err)
+	}
+	defer ws.Close()
+	for {
+		mt, message, err := ws.ReadMessage()
+
+		if err != nil {
+			log.Println("read: ", err)
+		}
+		log.Printf("recv: %s", message)
+
+		// write ws data
+		err = ws.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write: ", err)
+			break
+		}
+	}
+}
 
 func main() {
+	fmt.Println("Websocket Server")
 	r := gin.Default()
-	r.GET("/", processRequest)
+	r.GET("/ws", ws)
 	r.Run()
 }
 
